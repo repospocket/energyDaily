486f47a80fe3e97324d6bba2625c7f8f4a5b360c
repospocket <126033073+repospocket@users.noreply.github.com>
2023-03-11 17:53:14 -0500

    refactor

--- app/src/main/java/energyDaily/example/energyDaily/MainActivity.kt
+++ app/src/main/java/energyDaily/example/energyDaily/MainActivity.kt
@@ -3,7 +3,6 @@ package energyDaily.example.energyDaily
 import energyDaily.example.energyDaily.databinding.ActivityMainBinding
 import android.app.AlarmManager
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -28,15 +27,12 @@ class MainActivity : AppCompatActivity() {
     var datacolo = mutableMapOf<String, String>()
     val calendar = Calendar.getInstance()
     var dateNotes = mutableMapOf<String, String>()
-
-
-       private var alarmMgr: AlarmManager? = null
-        private lateinit var alarmIntent: PendingIntent
+    private var alarmMgr: AlarmManager? = null
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        title = "energyDaily";
+        title = "energyDaily"
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
@@ -53,58 +49,35 @@ class MainActivity : AppCompatActivity() {
         binding.calendarView.setMinimumDate(min)
         val calendarView = binding.calendarView
         calendarView.setMaximumDate(max)
-
         calendarView.setCalendarDayLayout(R.layout.cuscelll)
-
-
         calendarView.setOnDayClickListener(object : OnDayClickListener {
             override fun onDayClick(eventDay: EventDay) {
                 val clickedDayCalendar = eventDay.calendar
                 val timee = clickedDayCalendar.get(Calendar.YEAR).toString() + " " + clickedDayCalendar.get(Calendar.MONTH).toString() + " " + clickedDayCalendar.get(Calendar.DAY_OF_MONTH).toString()
-                if (dateNotes.get(timee).isNullOrEmpty()){
-                    val toast = Toast.makeText(applicationContext, "No note!", Toast.LENGTH_SHORT)
+                if (dateNotes[timee].isNullOrEmpty()){
+                    val toast = Toast.makeText(applicationContext, "No note!", Toast.LENGTH_LONG)
                     toast.show()
-
                     Handler(Looper.getMainLooper()).postDelayed({
                         toast.cancel()
-                    }, 500)
+                    }, 800)
 
                 }else{
                    // Toaster.makeLongToast(dateNotes.get(timee), 8000, baseContext);
-                    val toast = Toast.makeText(applicationContext, dateNotes.get(timee), Toast.LENGTH_SHORT)
+                    val toast = Toast.makeText(applicationContext, dateNotes[timee], Toast.LENGTH_LONG)
                     toast.show()
 
                     Handler(Looper.getMainLooper()).postDelayed({
                         toast.cancel()
-                    }, 500)
+                    }, 800)
 
                 }
             }
 
         })
 
-        /*
-        alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val alarmIntent1 = Intent(this, NotificationReceiver::class.java)
-        alarmIntent1.setAction("jaa")
-
-       val ala=    alarmIntent1.let { intent ->
-            PendingIntent.getBroadcast(this, 0, intent, 0)
-        }
-
-        alarmMgr?.set(
-                AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime() + 10 * 1000,
-                ala
-        )
-
-         */
-
-
-
         alarmMgr = this.getSystemService(ALARM_SERVICE) as AlarmManager
         val alarmIntent1 = Intent(this, NotificationReceiver::class.java)
-        alarmIntent1.setAction("jaa")
+        alarmIntent1.action = "jaa"
 
         val ala=    alarmIntent1.let { intent ->
             PendingIntent.getBroadcast(this, 0, intent,             PendingIntent.FLAG_IMMUTABLE)
@@ -122,29 +95,23 @@ class MainActivity : AppCompatActivity() {
             AlarmManager.INTERVAL_DAY,
             ala
         )
-
     }
 
-
-
-    fun submitNote(view: View) {
+    fun submitNote( @Suppress("UNUSED_PARAMETER") view: View) {
         val txt = binding.textnote.text.toString()
         val time = Calendar.getInstance()
         val year = time[Calendar.YEAR] // get the current year
         val month = time[Calendar.MONTH] // month...
         val day = time[Calendar.DAY_OF_MONTH] // current day in the month
 
-        dateNotes.put("$year $month $day", txt)
+        dateNotes["$year $month $day"] = txt
 
         saveDataa(dateNotes,"dataNotes")
         Toast.makeText(applicationContext, "            Note saved :)\n Tap on a day to show a note", Toast.LENGTH_SHORT).show()
 
-        this.recreate();
+        this.recreate()
 
     }
-
-
-
 
     fun tapcolor(view: View) {
         val cal = Calendar.getInstance()
@@ -156,7 +123,7 @@ class MainActivity : AppCompatActivity() {
             list1.removeAt(list1.size -1)
         }
 
-        when (view.getTag().toString()) {
+        when (view.tag.toString()) {
             "green" -> list1.add(
                     CalendarDay(calendar).apply {
                         backgroundDrawable = ContextCompat.getDrawable(baseContext, android.R.color.holo_green_light) })
@@ -174,119 +141,44 @@ class MainActivity : AppCompatActivity() {
 
         val myDate = "$year $month $day"
 
-        when (view.getTag().toString()) {
-            "green" ->         datacolo.put(myDate, "green")
+        when (view.tag.toString()) {
+            "green" -> datacolo[myDate] = "green"
 
-            "red" ->         datacolo.put(myDate, "red")
+            "red" -> datacolo[myDate] = "red"
 
-            "blue" ->         datacolo.put(myDate, "blue")
+            "blue" -> datacolo[myDate] = "blue"
             else -> print("otherwise")
         }
-/*
-        var  tst = ""
-        for ((k, v) in datacolo) {
-            tst += "$k = $v"
-        }
-        Log.d("OnTapp", tst);
-*/
         saveDataa(datacolo,"task list")
-
-
 
    }
 
-/*
-    fun tapcolor1(color: String) {
-        val cal = Calendar.getInstance()
-        val year = cal[Calendar.YEAR] // get the current year
-        val month = cal[Calendar.MONTH] // month...
-        val day = cal[Calendar.DAY_OF_MONTH] // current day in the month
-
-        if(datacolo.containsKey("$year $month $day")){
-            list1.removeAt(list1.size -1)
-        }
-
-            calendarView.setCalendarDays(list1)
-
-
-            val myDate = "$year $month $day"
-
-            when (color) {
-                "green" ->         datacolo.put(myDate, "green")
-
-                "red" ->         datacolo.put(myDate, "red")
-
-                "blue" ->         datacolo.put(myDate, "blue")
-                else -> print("otherwise")
-            }
-          //  saveData(datacolo)
-            //  Toast.makeText(this, "DONEEE", Toast.LENGTH_SHORT).show()
-
-        }
-*/
-
-
     private fun saveDataa( list: MutableMap<String, String>, sharedrStr: String) {
         val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
-       // val gson = Gson()
-     //   val json12 = sharedPreferences.getString( sharedrStr, null)
-     //   val type = object : TypeToken<MutableMap<String, String>>() {}.type
-/*
-        if(!json12.isNullOrEmpty()){
-            if (Type == "notes"){
-                dateNotes = Gson().fromJson<MutableMap<String, String>>(json12, type)
-            }else if (Type=="Colors"){
-                datacolo = Gson().fromJson<MutableMap<String, String>>(json12, type)
-            }
-        }
- */
-
         val editor = sharedPreferences.edit()
         val json = Gson().toJson(list)
         editor.putString(sharedrStr, json)
         editor.apply()
     }
-
-/*
-    private fun saveData( list: MutableMap<String, String>) {
-        val sharedPreferences = getSharedPreferences("shared preferences", Context.MODE_PRIVATE)
-        val gson = Gson()
-        val json12 = sharedPreferences.getString("task list", null)
-        val type = object : TypeToken<MutableMap<String, String>>() {}.type
-
-        if(!json12.isNullOrEmpty()){
-            datacolo = Gson().fromJson<MutableMap<String, String>>(json12, type)
-        }
-
-        val editor = sharedPreferences.edit()
-        val json = gson.toJson(list)
-        Log.d("SaveDataa", json);
-        editor.putString("task list", json)
-        editor.apply()
-    }
-*/
      private fun loadData() {
         val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
-        val gson = Gson()
+
         val json = sharedPreferences.getString("task list", null)
          val json2 = sharedPreferences.getString("dataNotes", null)
 
          val type = object : TypeToken<MutableMap<String, String>>() {}.type
 
          if(!json.isNullOrEmpty()){
-           datacolo = Gson().fromJson<MutableMap<String, String>>(json, type)
+           datacolo = Gson().fromJson(json, type)
 
          }
          if (!json2.isNullOrEmpty()){
-             dateNotes= Gson().fromJson<MutableMap<String, String>>(json2, type)
-
+             dateNotes= Gson().fromJson(json2, type)
          }
-
 
          var lolo =""
 
-         if (!datacolo.isNullOrEmpty()){
-             var  liss =  arrayListOf<CalendarDay>()
+         if (datacolo.isNotEmpty()){
 
              for ((k, v) in datacolo) {
              lolo += "$k = $v"
@@ -314,7 +206,7 @@ class MainActivity : AppCompatActivity() {
              binding.calendarView.setCalendarDays(list1)
 
          }
-         Log.d("loadDataa", lolo);
+         Log.d("loadDataa", lolo)
      }
 
 
--- app/src/main/java/energyDaily/example/energyDaily/NotificationReceiver.java
+++ app/src/main/java/energyDaily/example/energyDaily/NotificationReceiver.java
@@ -5,18 +5,48 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
 
 public class NotificationReceiver extends BroadcastReceiver {
 
     private NotificationManagerCompat notificationManager;
 
+
+    public void saveData(String day, String co, Context context) {
+        SharedPreferences sharedPreferences1 = context.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+        String json1 = sharedPreferences1.getString("task list", null);
+        Type type = new TypeToken<HashMap<String, String>>() {
+        }.getType();
+        HashMap<String, String> datacolo = new HashMap<>();
+
+        if (json1 != null && !json1.isEmpty()) {
+            datacolo = new Gson().fromJson(json1, type);
+        }
+
+        datacolo.put(day, co);
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(datacolo);
+        Log.d("SaveDataa", json);
+        editor.putString("task list", json);
+        editor.apply();
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -27,30 +57,28 @@ public class NotificationReceiver extends BroadcastReceiver {
         int day = java.util.Calendar.getInstance().get(java.util.Calendar.DAY_OF_MONTH);
         int month = java.util.Calendar.getInstance().get(java.util.Calendar.MONTH);
         int year = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
-        String lala = Integer.toString(year) + " " + Integer.toString(month) + " " + Integer.toString(day);
-
-        gg sa = new gg();
+        String lala = year + " " + month + " " + day;
 
         if (notificationManager == null)
             notificationManager = NotificationManagerCompat.from(context);
 
         switch (intent.getAction()) {
             case "blue":
-                sa.saveData(lala, "blue", context);
+                saveData(lala, "blue", context);
                 Toast.makeText(context, "Normal energy..", Toast.LENGTH_SHORT).show();
                 notificationManager.cancelAll();
 
 
                 break;
             case "red":
-                sa.saveData(lala, "red", context);
+                saveData(lala, "red", context);
                 Toast.makeText(context, "Low energy..", Toast.LENGTH_SHORT).show();
                 notificationManager.cancelAll();
 
                 break;
             case "green":
-                sa.saveData(lala, "green", context);
-                Toast.makeText(context, "High energy today..", Toast.LENGTH_SHORT).show();
+                saveData(lala, "green", context);
+                Toast.makeText(context, "High energy..", Toast.LENGTH_SHORT).show();
                 notificationManager.cancelAll();
 
                 break;
@@ -67,22 +95,22 @@ public class NotificationReceiver extends BroadcastReceiver {
 
         Intent activityIntent = new Intent(context, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(context,
-                0, activityIntent, 0);
+                0, activityIntent, PendingIntent.FLAG_IMMUTABLE);
         Intent broadcastIntent = new Intent(context, NotificationReceiver.class);
         broadcastIntent.setAction("blue");
         PendingIntent actionIntent = PendingIntent.getBroadcast(context,
-                0, broadcastIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+                0, broadcastIntent, PendingIntent.FLAG_IMMUTABLE);
 
         Intent broadcastIntent2 = new Intent(context, NotificationReceiver.class);
         broadcastIntent2.setAction("red");
 
         PendingIntent actionIntent2 = PendingIntent.getBroadcast(context,
-                0, broadcastIntent2, PendingIntent.FLAG_UPDATE_CURRENT);
+                0, broadcastIntent2, PendingIntent.FLAG_IMMUTABLE);
 
         Intent broadcastIntent3 = new Intent(context, NotificationReceiver.class);
         broadcastIntent3.setAction("green");
         PendingIntent actionIntent3 = PendingIntent.getBroadcast(context,
-                0, broadcastIntent3, PendingIntent.FLAG_UPDATE_CURRENT);
+                0, broadcastIntent3, PendingIntent.FLAG_IMMUTABLE);
 
         Notification notification = new NotificationCompat.Builder(context, "channel1")
                 .setSmallIcon(R.drawable.ic_face)
@@ -92,8 +120,8 @@ public class NotificationReceiver extends BroadcastReceiver {
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentIntent(contentIntent)
                 .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher, "Bad", actionIntent2)
-                .addAction(R.mipmap.ic_launcher, "Neutral", actionIntent)
+                .addAction(R.mipmap.ic_launcher, "Low", actionIntent2)
+                .addAction(R.mipmap.ic_launcher, "Normal", actionIntent)
                 .addAction(R.mipmap.ic_launcher, "Good", actionIntent3)
                 .setAutoCancel(true)
                 .build();
@@ -101,9 +129,8 @@ public class NotificationReceiver extends BroadcastReceiver {
         if (notificationManager == null)
             notificationManager = NotificationManagerCompat.from(context);
 
-
-        notificationManager.notify(1, notification);
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, notification);
+        }
     }
-
-
 }
